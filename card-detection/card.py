f077b0d684ce0dcb72f29843839fd4704a6a3c7c
hxtruong <hoangxuantruong98@gmail.com>
2019-09-26 20:19:35 +0700

    feat: complete card class with check size

--- card-detection/card.py
+++ card-detection/card.py
@@ -3,18 +3,17 @@ from utilsImage import *
 # kích thước 85,6 mm x 53,98 mm
 STANDARD_WIDTH = 856
 STANDARD_HEIGHT = 539.8
-STANDARD_W_H_SIZE_RATIO = STANDARD_WIDTH/STANDARD_HEIGHT
+STANDARD_W_H_SIZE_RATIO = STANDARD_WIDTH / STANDARD_HEIGHT
 
-MAX_WIDTH = 1000
 
 class Card:
     def __init__(self, originImage):
         self.originImage = originImage
         self.image = None
-        
+
     def __str__(self):
         return str(self.__class__) + ": " + str(self.__dict__)
-    
+
     def getGreenColor(self, img):
         # Convert BGR to HSV
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -28,7 +27,7 @@ class Card:
         res = cv2.bitwise_and(img, img, mask=mask)
         res = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
         return res
-    
+
     def getGreenEdges(self, img):
         green_img = self.getGreenColor(img)
 
@@ -47,12 +46,12 @@ class Card:
         #     Get edges
         green_edges = cv2.Canny(green_img, 50, 120)
         return green_edges
-    
+
     def getCardBorder(self, img):
         img = histogramEqualize(img)
         green_edges = self.getGreenEdges(img)
         contours, hierarchy = cv2.findContours(green_edges, cv2.RETR_EXTERNAL,
-                                              cv2.CHAIN_APPROX_SIMPLE)
+                                               cv2.CHAIN_APPROX_SIMPLE)
 
         contours = sorted(contours, key=cv2.contourArea, reverse=True)
 
@@ -70,27 +69,122 @@ class Card:
                 #             drawContourBoundary(img.copy(), cnt)
                 break
         return x, y, w, h
-    
+
     def isExsitBorder(self):
-        x,y,w,h = self.getCardBorder(self.originImage)
-        if x!= None and y!= None and w!=None and h!=None:
-            self.border = (x,y,w,h)
-            self.image = croppingStandardImage(self.originImage, x,y,w,h, width = STANDARD_WIDTH)
+        x, y, w, h = self.getCardBorder(self.originImage)
+        if x != None and y != None and w != None and h != None:
+            self.border = (x, y, w, h)
+            self.image = croppingStandardImage(self.originImage,
+                                               x,
+                                               y,
+                                               w,
+                                               h,
+                                               width=STANDARD_WIDTH)
             return True
         return False
-    
-    def isStandardSizeRatio(self, threshold = 0.1, standard_w_h = STANDARD_W_H_SIZE_RATIO):
-        h,w = self.image.shape[:2]
-        self.width_height_ratio = w/h
-        return abs(self.width_height_ratio-standard_w_h) <= threshold
-    
-    def cropQh(self):
-        qh_threshold = 10
+
+    def isStandardSizeRatio(self,
+                            threshold=0.1,
+                            standard_w_h=STANDARD_W_H_SIZE_RATIO):
+        h, w = self.image.shape[:2]
+        self.width_height_ratio = w / h
+        return abs(self.width_height_ratio - standard_w_h) <= threshold
+
+    def cropQh(self, qhThreshold=10):
         minX = 70
         minY = 50
         qhDiameter = 180  # quoc huy co duong kinh la 14mm
 
-        self.qh = self.image[minY - qh_threshold:minY + qhDiameter + qh_threshold, minX -
-                       qh_threshold:minX + qhDiameter + qh_threshold]
-        return self.qh
-    
+        self.originQh = self.image[minY - qhThreshold:minY + qhDiameter +
+                                   qhThreshold, minX - qhThreshold:minX +
+                                   qhDiameter + qhThreshold]
+        return self.originQh
+
+    def isExsitQhContour(self):
+        def getQhColor(img):
+            # Convert BGR to HSV
+            hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+            #     showImg(hsv, "color")
+            # cv.imshow("hsv", hsv)
+            # define range of color in HSV
+
+            ## Gen lower mask (0-5) and upper mask (175-180) of RED
+            mask1 = cv2.inRange(hsv, (0, 50, 20), (35, 255, 255))
+            mask2 = cv2.inRange(hsv, (170, 50, 20), (180, 255, 255))
+
+            ## Merge the mask and crop the red regions
+            mask = cv2.bitwise_or(mask1, mask2)
+
+            res = cv2.bitwise_and(img, img, mask=mask)
+            res = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
+            return res
+
+        def getEdgeImage(img):
+            kernel = np.ones((1, 1), np.uint8)
+            img = cv2.dilate(img, kernel, iterations=1)
+            #     showImg(img, 'edge enhance QH')
+            img = cv2.GaussianBlur(img, (3, 3), cv2.BORDER_DEFAULT)
+            # showImg(img, 'edge blur QH')
+
+            # Get edges
+            edges = cv2.Canny(img, 50, 120)
+            # showImg(qhEdges, 'edge QH')
+            return edges
+
+        def cropStandardQh(qh, qhCountour, rQh=71):
+            # calculate moments of binary image
+            M = cv2.moments(qhCountour)
+
+            # calculate x,y coordinate of center
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+            return qh[cY - rQh:cY + rQh, cX - rQh:cX + rQh]
+
+        self.cropQh()
+        self.colorQh = getQhColor(self.originQh)
+        self.grayQh = cv2.cvtColor(self.colorQh, cv2.COLOR_BGR2GRAY)
+        originQhThresh = getEdgeImage(self.grayQh)
+
+        # ret, originQhThresh = cv2.threshold(qh_gray, 127, 255, 0)
+
+        # Extract contours from second target image
+        contours, hierarchy = cv2.findContours(originQhThresh,
+                                               cv2.RETR_EXTERNAL,
+                                               cv2.CHAIN_APPROX_NONE)
+        contours = sorted(contours, key=cv2.contourArea, reverse=True)
+        print("Length contours of QH: ", len(contours))
+
+        self.qhCountour = None
+        if len(contours) == 1:
+            self.qhCountour = contours[0]
+            self.standardQh = cropStandardQh(self.originQh, self.qhCountour)
+        else:
+            return False
+        return True
+
+    def isMatchQhTemplate(self,
+                          thresholdMatch=0.1,
+                          method='cv2.TM_CCOEFF_NORMED'):
+        # Getting quoc huy
+        self.templateQh = cv2.imread('standardQhTemplate.png')
+        self.templateGrayQh = cv2.cvtColor(self.templateQh, cv2.COLOR_BGR2GRAY)
+
+        try:
+            self.grayQh
+        except NameError:
+            self.grayQh = None
+
+        if self.grayQh is None:
+            if not self.isExsitQhContour():
+                print("Not exsit contour")
+                return False
+
+        method = eval(method)
+        # Apply template Matching
+        res = cv2.matchTemplate(self.grayQh, self.templateGrayQh, method)
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+
+        if min_val > thresholdMatch:
+            print("Min Val: ", min_val)
+            return False
+        return True
